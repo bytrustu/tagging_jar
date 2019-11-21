cec740403803480655fd3d5f3beb10fe05c33afc
bytrustu <bytrustu@gmail.com>
2019-11-21 18:39:34 +0900

    v1.0.34

--- app.js
+++ app.js
@@ -42,27 +42,28 @@ app.all("*", function(req, res, next){
 
 console.log('start');
 // const url = req.body.url;
-var childProcess = require('child_process');
-var options = {maxBuffer:1024*1024*100, encoding:'utf8', timeout:5000};
-var child = childProcess.spawn('java -jar ./youtube/Tagging.jar https://www.youtube.com/watch?v=0u8o-C7nCcs ./public/youtube/img ./public/youtube/blank', options);
-child.stdout.on('data', (d) => {
-  console.log(d.toString())
-})
-child.stdout.on('data', (d) => {
-  console.log(d.toString())
-})
-child.('error', (e) => {
-  console.log(e.toString())
-})
+// var spawn = require('child_process').spawn, ls = spawn('ls', ['-a']);
+// var options = {maxBuffer:1024*1024*100, encoding:'utf8', timeout:5000};
+// spawn('java -jar ./youtube/Tagging.jar https://www.youtube.com/watch?v=PoKpbxa96pE ./public/youtube/img ./public/youtube/blank', options)
+// var child = childProcess.spawn('java -jar ./youtube/Tagging.jar https://www.youtube.com/watch?v=0u8o-C7nCcs ./public/youtube/img ./public/youtube/blank', options);
+// child.stdout.on('data', (d) => {
+//   console.log(d.toString())
+// })
+// child.stdout.on('data', (d) => {
+//   console.log(d.toString())
+// })
+// child.('error', (e) => {
+//   console.log(e.toString())
+// })
 
-// child = exec(`java -jar ./youtube/Tagging.jar https://www.youtube.com/watch?v=lwd-wjDQQPY ./public/youtube/img ./public/youtube/blank`,
-// function (error, stdout, stderr){
-// 	console.log('stdout: ' + stdout);
-// 	console.log('stderr: ' + stderr);
-// 	if(error !== null){
-// 		console.log('exec error: ' + error);
-// 	}
-// });
+child = exec(`java -jar ./youtube/Tagging.jar https://www.youtube.com/watch?v=PoKpbxa96pE ./public/youtube/img ./public/youtube/blank`,
+function (error, stdout, stderr){
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if(error !== null){
+		console.log('exec error: ' + error);
+	}
+});
 
 
 
