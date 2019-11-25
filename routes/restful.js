552f2f4509c24fb308995446bebfcbf0ba0e8de5
bytrustu <bytrustu@gmail.com>
2019-11-26 00:23:11 +0900

    1

--- routes/restful.js
+++ routes/restful.js
@@ -23,14 +23,11 @@ module.exports.active_process = function(req, res){
 	console.log(url)
 	console.log(__dirname);
 	const link = `https://www.youtube.com/watch?v=${url}`;
-	child = exec(`java -jar ${__dirname}/../youtube/Tagging.jar ${link} ${__dirname}/../public/youtube/blank C:/git/tagging_jar/public/youtube/img`,
+	child = exec(`java -jar /tagging_jar/youtube/Tagging.jar ${link} /tagging_jar/public/youtube/blank /tagging_jar/public/youtube/img`,
 	function (error, stdout, stderr){
-		send(res, 200, true);
+		console.log(error);
+		console.log('end');
 	});
 
-	setTimeout(()=>{
-		send(res, 200, true);
-		return;
-	},1000)
 	
 };
